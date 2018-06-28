d971eaaa19a5e9be8f300f54d3450ab8d81c4246
Nick George <nicholas.george32@gmail.com>
2018-06-28 14:58:26 -0600

    started new script for taking an ROI over a directory of images.

---
+++ take-roi.py
@@ -0,0 +1,4 @@
+# a simple script to apply the same ROI to a directory full of images.
+# select the image directoy
+# select the roi
+# using imagej script parameters(https://imagej.net/Script_Parameters) for the gui on this one.
