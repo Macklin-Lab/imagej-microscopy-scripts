a20445f8120c8263b1aedafb410f1f6b3fc1c823
Nick George <nicholas.george32@gmail.com>
2018-10-22 12:09:35 -0600

    added file endings for blind files. Added logging. updated readme.

--- Lab-utility-plugins/blind-files_.py
+++ Lab-utility-plugins/blind-files_.py
@@ -1,18 +1,19 @@
 # blind files
 # Author: Nick George
 # Contact: nicholas.m.george@ucdenver.edu
-# updated last: 2018-06-29
+# updated last: 2018-10-22
 # using imagej script parameters(https://imagej.net/Script_Parameters) for the gui on this one.
 # script params below
 #@ File (label="Choose the image directory to blind", style="directory") directory
-#@ String (label="What is the file ending of your image files?", choices={".tif",".jpg",".jpeg",".png", ".pdf"}, style="listBox") fileend
-
+#@ String (label="What is the file ending of your image files?", choices={".tif",".jpg",".jpeg",".png", ".pdf", ".czi", ".lsm", ".nd2", ".lif", ".oib"}, style="listBox") fileend
 import os
 import uuid
 import shutil
 import sys
+from ij import IJ
 
-
+IJ.log("\n\n >> Welcome to blind_files! <<" )
+IJ.log("\n\n >> Please report issues here: \nhttps://github.com/Macklin-Lab/imagej-microscopy-scripts/issues")
 folder = str(directory)
 assert os.path.exists(folder), "Couldnt find the directory. Try using 'Browse' next time"
 
@@ -35,9 +36,13 @@ def copy_files(old, new):
 	copies the old to the new.
 	"""
 	if os.path.exists(new):
-		print("file " + new + " exists, continuing")
+		message = str("\n >> *file:* " + new + " exists, continuing")
+		IJ.log(message)
+		print(message)
 	else:
-		print("original file "+ old+ " saving as " + new)
+		message = str("\n >> *original file* \n --> "+ old+ "\n >> *saving as* \n --> " + new)
+		print(message)
+		IJ.log(message)
 		shutil.copyfile(old, new)
 
 
@@ -45,18 +50,23 @@ def copy_files(old, new):
 
 # make new directory
 new_dir = make_dir(folder)
-
+message = "\n\n >> Making new directory \n"+ str(new_dir) 
+IJ.log(message)
 # create key file name based on input directory.
 key_file = os.path.join(folder, "KEY.csv")
 # make blinded key path
 
 blinded_key = os.path.join(new_dir, "blinded-key.csv")
 
+message = "\n\n >> Writing key "+ str(blinded_key) 
+IJ.log(message)
+
 listoffiles = [f for f in os.listdir(folder) if f.endswith(fileend)]
 
 # ignore hidden files starting with .
 real_names = [os.path.join(folder,f) for f in listoffiles if not f.startswith(".")]
-
+message = "\n\n >> Now blinding the following files: \n"+ "\n --> ".join(real_names) 
+IJ.log(message)
 # make unique blinded names use a set comprehension to avoid duplicates
 
 uniquenames = {os.path.join(new_dir, str(uuid.uuid4().hex)+ fileend) for old_name in real_names}
@@ -71,6 +81,7 @@ print("writing key")
 # if a key file already exists, quit.
 if os.path.exists(key_file):
 	print("key exists, quitting")
+	IJ.log("\n\n>> !!key exists, quitting!\n\n")
 	sys.exit("goodbye!")
 
 # iterate through the zipped list of new and old files, copying them to blinded
@@ -93,3 +104,4 @@ with open(blinded_key, "w") as blindKey:
 	       	blindKey.write(new + "\n")
 
 print("DONE")
+IJ.log("\n\nDONE!\n\n")
--- Lab-utility-plugins/tif-convert_.py
+++ Lab-utility-plugins/tif-convert_.py
@@ -1,7 +1,7 @@
 #tif-convert.py
 # Author: Nick George
 # Contact: nicholas.m.george@ucdenver.edu
-# updated last: 2018-06-29
+# updated last: 2018-10-22
 # using imagej script parameters(https://imagej.net/Script_Parameters) for the gui on this one.
 # script params below
 #@ File (label="Choose the image directory to convert to tif.", style="directory") directory
@@ -16,7 +16,8 @@ import os
 ## this is a list of files I have tested it with and it seems to work with.
 ## let me know if you want others included,
 accepted_files = (".czi", ".lsm", ".lif", ".oib", ".nd2")
-
+IJ.log("\n\n >> Welcome to tif-convert! <<" )
+IJ.log("\n >> Please report issues here: \nhttps://github.com/Macklin-Lab/imagej-microscopy-scripts/issues\n\n")
 
 # open directoy
 folder = str(directory)
@@ -28,6 +29,8 @@ def make_dir(workingDir):
 	make new file directory for extracted tifs
 	"""
 	new_dir = os.path.join(workingDir, "converted_tif")
+	message = "\n>> Files will be saved: \n --> "+str(new_dir)
+	IJ.log(message)
 	if os.path.exists(new_dir):
 		return new_dir
 	else:
@@ -73,6 +76,8 @@ def save_tif(imps, name, new_dir):
 	"""
 	new_name = os.path.join(new_dir, name+".tif")
 	if os.path.exists(new_name):
+		message = ">> *File:* \n -->"+ new_name + "\n exists, continuing"
+		IJ.log(message)
 		print("File "+ new_name + " exists, continuing")
 		return
 	else:
@@ -86,9 +91,14 @@ new_dir = make_dir(folder)
 listoffiles = [ str(f) for f in os.listdir(folder) if f.endswith(accepted_files)]
 # ignore automatically generated hidden files that start with .
 real_names = [f for f in listoffiles if not f.startswith(".")]
+message = "\n\n >> Now converting the following files: \n"+ "\n --> ".join(real_names) 
+IJ.log(message)
+
 for i in real_names:
 	image = os.path.join(folder, i)
 	print(image)
+	message = "\n >> Now processing: \n --> " +str(image)
+	IJ.log(message)
 	reader = ImageReader()
 	# set image id
 	reader.setId(image)
@@ -101,3 +111,4 @@ for i in real_names:
 	       	save_tif(imps, fixed_name, new_dir)
 
 print("DONE!")
+IJ.log("\n\n >> DONE!")
